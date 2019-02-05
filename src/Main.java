da9f3c67b04ebc97a6e446075107f844961fcc22
RobsonMoura <mourarob@hotmail.com>
2019-02-05 11:23:30 -0200

    uri1001

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, x;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		
+		x = A + B;
+		
+		System.out.println("X = " + x);
+		
+	
+		sc.close();
+	}
+
+}
